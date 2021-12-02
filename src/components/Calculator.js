b08f70b9f0316150c3cc6a349f80722e69d3f963
Uzair Ali <uzairali19b@gmail.com>
2021-12-02 19:00:52 +0500

    update setup files

---
+++ src/components/Calculator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const calValues = [
+  ['C', '+-', '%', '/'],
+  [7, 8, 9, 'X'],
+  [4, 5, 6, '-'],
+  [1, 2, 3, '+'],
+  [0, '.', '='],
+];
+
+class Calculator extends React.Component {
+  render() {
+    return <div className="container">Hello world</div>;
+  }
+}
+
+export default Calculator;
