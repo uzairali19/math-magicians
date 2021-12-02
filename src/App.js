08f2d98f173f0044f5c0c88e49f21f9202be39f1
Uzair Ali <uzairali19b@gmail.com>
2021-12-02 20:10:04 +0500

    update calculator UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Calculator from './components/Calculator';
+import './style.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <h1>React Calculator</h1>
+      <p>By Uzair Ali</p>
       <Calculator />
     </div>
   );
