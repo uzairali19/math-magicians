eab30c70317e05e04abb96f87f60576f20f2f57c
Uzair Ali <uzairali19b@gmail.com>
2021-12-02 20:45:05 +0500

    convert app.js to class based component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import './style.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>React Calculator</h1>
-      <p>By Uzair Ali</p>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>React Calculator</h1>
+        <p>By Uzair Ali</p>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
